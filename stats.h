44a946830420fe0a214e1f310ba21ec74cec559d
Jon Gilvey <gilvey.jonathan@gmail.com>
2018-03-18 20:41:08 +0100

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -1,40 +1,172 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for first week assignment. 
  *
- * <Add Extended Description Here>
+ * First week assignments header file for the course "Introduction to Embedded Systems Software and Development Environments".
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Jon Gilvey
+ * @date 3/18/2018
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
-/**
- * @brief <Add Brief Description of Function Here>
+/* No comments about declarations */ 
+
+
+ /**
+
+ * @brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
+
  *
- * <Add Extended Description Here>
+
+ * The function takes the array and passes it to the functions "find_minimum", "find_maximum", "find_mean" and "find_median". The function then prints out the values these other functions return. 
+
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return The function does not return a value
+
  */
+
+ void print_statistics(unsigned char* ucptr, unsigned int n);
+
+ 
+
+ /**
+
+ * @brief Given an array of data and a length, prints the array to the screen
+
+ *
+
+ * The function will take the array and then print out the values individually separated by commas. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return The function does not return a value
+
+ */
+
+void print_array(unsigned char* ucptr, unsigned int n);
+
+
+
+ /**
+
+ * @brief Given an array of data and a length, returns the median value
+
+ *
+
+ * The function will take the array, sort the array and then find the median of the array based off the number of elements in the array. The function will call "sort_array" to perform an initial sort. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return The median of the array, the data type will be unsigned char. 
+
+ */
+
+ unsigned char find_median(unsigned char* ucptr, unsigned int n);
+
+
+
+ /**
+
+ * @brief Given an array of data and a length, returns the mean.
+
+ *
+
+ * The function will return a floating number which will be the mean. This will be done by summing up the values in the array and then dividing by the total number of elements. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return Floating point number giving the mean. 
+
+ */
+
+ float find_mean(unsigned char* ucptr, unsigned int n);
+
+ 
+
+ /**
+
+ * @brief Given an array of data and a length, returns the maximum.
+
+ *
+
+ * The function will first call the "sort_array" function and return the zeroth value. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return An unsigned char which is the miniumum value of the array. 
+
+ */
+
+ unsigned char find_maximum(unsigned char* ucptr, unsigned int n);
+
+
+
+ /**
+
+ * @brief Given an array of data and a length, returns the minimum
+
+ *
+
+ * The function will first call the "sort_array" function and return the "n-1" value. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return An unsigned char which is the miniumum value of the array. 
+
+ */
+
+ unsigned char find_minimum(unsigned char* ucptr, unsigned int n);
+
+ 
+
+ /**
+
+ * @brief Given an array of data and a length, sorts the array from largest to smallest. 
+
+ *
+
+ * The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. This will be implemented using a "bubble sort" algorithm. 
+
+ *
+
+ * @param ucptr	The unsigned char pointer which points to an n-element data array.
+
+ * @param n An unsigned integer which gives the size of the array pointed to by "ucptr" 
+
+ * @return This function will return a pointer to the reordered array (I know this is redundant). 
+
+ */
+
+ unsigned char* sort_array(unsigned char* ucptr, unsigned int n);
 
 
 #endif /* __STATS_H__ */
